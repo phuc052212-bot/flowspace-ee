778d69d522eb4845cc0c7b85a0b2f9de642f8acd
NguyenQuocHuii <nguyenquochuy.nqh05@gmail.com>
2026-07-19 16:23:53 +0700

    fix: switch from UseSqlServer to UseSqlite

--- backend/src/FlowSpace.Persistence/DependencyInjection.cs
+++ backend/src/FlowSpace.Persistence/DependencyInjection.cs
@@ -12,7 +12,7 @@ namespace FlowSpace.Persistence
         public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<FlowSpaceDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                options.UseSqlite(configuration.GetConnectionString("DefaultConnection"),
                     b => b.MigrationsAssembly(typeof(FlowSpaceDbContext).Assembly.FullName)));
 
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));

5b4a44e7e74f041b41722944ce24f17d10626268
NguyenQuocHuii <nguyenquochuy.nqh05@gmail.com>
2026-07-19 16:09:48 +0700

    fix: import missing namespace in DocumentsController

--- backend/src/FlowSpace.Api/Controllers/DocumentsController.cs
+++ backend/src/FlowSpace.Api/Controllers/DocumentsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using FlowSpace.Application.Common.Dtos;
 
 namespace FlowSpace.Api.Controllers
 {

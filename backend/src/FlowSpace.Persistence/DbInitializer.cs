7670cc28ca9340cf11182dc223397acdb7af82e4
NguyenQuocHuii <nguyenquochuy.nqh05@gmail.com>
2026-07-20 15:11:26 +0700

    fix: import missing EF core namespace in DbInitializer

--- backend/src/FlowSpace.Persistence/DbInitializer.cs
+++ backend/src/FlowSpace.Persistence/DbInitializer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using FlowSpace.Domain.Entities;
 using FlowSpace.Domain.Enums;
 using FlowSpace.Persistence.Contexts;
